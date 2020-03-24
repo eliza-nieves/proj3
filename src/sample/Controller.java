aaa5ab2dead61260c5d8ba7ffc8cc51fa44c3a8d
Eliza Nieves <snowfell00@gmail.com>
2020-03-24 14:29:02 -0400

    Added sample controller method

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+
+    @FXML
+    private TextArea printed;
+
+    public void addBtnPress(ActionEvent event){
+    printed.appendText("hello\n");
+    }
 }
+
+
