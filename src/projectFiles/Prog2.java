576c38a284acb89ba14c34daf50a1236a93fb343
Eliza Nieves <snowfell00@gmail.com>
2020-03-26 14:52:41 -0400

    Readded Prog2 and TuitionManager; turns out you need to turn them in too alongside the new GUI. mb

---
+++ src/projectFiles/Prog2.java
@@ -0,0 +1,14 @@
+package projectFiles;
+
+/**
+ Container class to run TuitionManager.java
+ @author Eliza Nieves
+ @author Raheel Ozair
+ */
+public class Prog2
+{
+    public static void main(String [] args)
+    {
+        new TuitionManager().run();
+    }
+}
